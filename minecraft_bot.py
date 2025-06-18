f45460491ae1ca313ca1ce2b217fe75d0252072d
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-06-18 15:45:34 -0300

    treino

--- minecraft_bot.py
+++ minecraft_bot.py
@@ -4,6 +4,20 @@ from chatterbot.trainers import ListTrainer
 # Criando o bot
 bot = ChatBot("MinecraftBot")
 
+# Texto para treinar o bot
+bot.train([
+'Oi',
+'Olá!',
+'Eu quero fazer uma farm',
+'Me diga qual farm que você quer fazer',
+'Uma farm de obsidian',
+'Para farmar obsidian a melhor maneira de obte-la é no The end, com um beacon com haste II e uma picareta de netherite com eficiencia 5, para minerar todos os pilares do end.',
+'Como pegar netherine?',
+'No Nether, com uma picareta de diamante com eficiencia 2, vá até a camada 13 para minerar, ou faça várias camas para explodi-las, já que no nether não é possivel dormir.'
+'Como fazer uma farm de lã?'
+'Primeiro encontre uma ovelha, prenda ela e coloque um observador na grama de baixo dela, depois lique o sinal de redstone em um dispenser para coletar a lã e coloque um carrinho com funil para a coleta.'
+])
+
 # Lendo o arquivo de texto com perguntas e respostas
 with open("conversas.txt", "r", encoding="utf-8") as arquivo:
     linhas = [linha.strip() for linha in arquivo if linha.strip()]
